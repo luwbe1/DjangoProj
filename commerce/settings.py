174c4689c5a5afe65cadb36494e77aafb2ab3acf
YuBin Kim <43837921+luwbe1@users.noreply.github.com>
2021-12-20 11:10:28 +0900

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cpv0gcadk))d+v0bx2=uv$-f+kmqa&9y-mhajcyc^1vtk*=cpz')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
